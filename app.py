d3f797592eeff200a448abc1646523ea8d1c1045
Melvin Perello <izumiiii.10@gmail.com>
2018-12-18 02:29:18 +0000

    added fix for error propagation

--- app.py
+++ app.py
@@ -9,7 +9,19 @@ db = SQLAlchemy(app)
 
 # Create API's Endpoint
 from flask_restful import Api
-api = Api(app)
+#-------------------------------------------------------------------------------
+# FIX for Flask Exception Propagation
+class CustomApi(Api):
+    def handle_error(self, e):
+        for val in app.error_handler_spec.values():
+            for handler in val.values():
+                registered_error_handlers = list(filter(lambda x: isinstance(e, x), handler.keys()))
+                if len(registered_error_handlers) > 0:
+                    raise e
+        return super.handle_error(e)
+#-------------------------------------------------------------------------------
+#API
+api = CustomApi(app)
 
 # /person resources
 from resources import PersonListResource,PersonResource
